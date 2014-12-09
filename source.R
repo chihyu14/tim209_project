c32a73b8f6f7d9a5c973c361966e9011b666b842
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-12-08 18:51:46 -0800

    Added initial function for decision trees

--- source.R
+++ source.R
@@ -1,8 +1,8 @@
 #Preprocessing 
 
-train=read.csv("~/R/kdd99/kddcup.data_10_percent_corrected",header=FALSE)
-test=read.csv("~/R/kdd99/kddcup.newtestdata_10_percent_unlabeled",header=FALSE)
-header=read.csv("~/R/kdd99/colnames",header=FALSE)
+train=read.csv("~/Documents/UCSC/Grad/Mach_Learn/R/project/idsTrainSmall.txt",header=FALSE)
+test=read.csv("~/Documents/UCSC/Grad/Mach_Learn/R/project/idsTestSmall.txt",header=FALSE)
+header=read.csv("~/Documents/UCSC/Grad/Mach_Learn/R/project/colnames",header=FALSE)
 names(train)=header[,1]
 names(test)=header[,1]
 names(train)[42]="status"
@@ -44,6 +44,9 @@ train.normal=train[train[,43]=="normal.",]
 
 
 
+# Decision trees
+attach(train)
+ids_tree <- tree(status ~ duration+flag+protocol_type+src_bytes+dst_bytes+land+wrong_fragment+urgent+hot+num_failed_logins+logged_in+num_compromised+root_shell+su_attempted+num_root+num_file_creations+num_shells+num_access_files+num_outbound_cmds+is_host_login+is_guest_login+count+srv_count+serror_rate+srv_serror_rate+rerror_rate+srv_rerror_rate+same_srv_rate+diff_srv_rate+srv_diff_host_rate+dst_host_count+dst_host_srv_count+dst_host_same_srv_rate+dst_host_diff_srv_rate+dst_host_same_src_port_rate+dst_host_srv_diff_host_rate+dst_host_serror_rate+dst_host_srv_serror_rate+dst_host_rerror_rate+dst_host_srv_rerror_rate, train)
 
 
 
