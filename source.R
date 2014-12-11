6290d5f38ebb1ae596ee3c93f97e4fe029321fee
chihyu(Bruce) <chihyu14@users.noreply.github.com>
2014-12-11 14:20:02 -0800

    Update source.R

--- source.R
+++ source.R
@@ -59,6 +59,9 @@ table(knn.pred, train[,42])
 attach(train)
 ids_tree <- tree(status ~ duration+flag+protocol_type+src_bytes+dst_bytes+land+wrong_fragment+urgent+hot+num_failed_logins+logged_in+num_compromised+root_shell+su_attempted+num_root+num_file_creations+num_shells+num_access_files+num_outbound_cmds+is_host_login+is_guest_login+count+srv_count+serror_rate+srv_serror_rate+rerror_rate+srv_rerror_rate+same_srv_rate+diff_srv_rate+srv_diff_host_rate+dst_host_count+dst_host_srv_count+dst_host_same_srv_rate+dst_host_diff_srv_rate+dst_host_same_src_port_rate+dst_host_srv_diff_host_rate+dst_host_serror_rate+dst_host_srv_serror_rate+dst_host_rerror_rate+dst_host_srv_rerror_rate, train)
 
+class(tree)
+train$service=as.integer(train$service) //convert service into numeric since its level over 32.
+ids_tree=tree(train$status~.,train)
 
 
                       
